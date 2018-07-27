c1a99f963b5de40d22cbed14a90755e228932aa5
ontofractal <ontofractal@protonmail.com>
2018-07-27 11:34:55 +0100

    add Address.public_key fn and test

--- lib/waveschainex/addresses.ex
+++ lib/waveschainex/addresses.ex
@@ -32,4 +32,20 @@ defmodule Waveschainex.Address do
   def data(client, address, _opts \\ []) do
     get(client, "/addresses/data/#{address}")
   end
+
+  @doc """
+  Generate a address from public key
+
+  ## Parameters
+
+  - client : Tesla client
+  - public_key (String.t): Public key Base58-encoded
+  - opts: Optional parameters
+
+  """
+  @spec public_key(Tesla.Env.client(), String.t(), keyword()) ::
+          {:ok, Tesla.Env.t()} | {:error, Tesla.Env.t()}
+  def public_key(client, public_key, _opts \\ []) do
+    get(client, "/addresses/publicKey/#{public_key}")
+  end
 end
--- test/address_test.exs
+++ test/address_test.exs
@@ -17,10 +17,17 @@ defmodule Waveschainex.AddressTest do
   end
 
   test "get data for address", %{client: client} do
-    # FIXME: update address to one that has data transactions on mainnet 
+    # FIXME: update address to one that has data transactions on mainnet
     address = "3PAfgHQPgodM6MxUzqRkepiKofGnECNoxt5"
     {:ok, env} = data(client, address)
 
     assert env.body == []
+  end
+
+  test "get address from public key", %{client: client} do
+    pkey = "9u6mN4kN8U7iQVsyNaru7ko6VV3E3pQ6sqcmmDwM6k7a"
+    {:ok, env} = public_key(client, pkey)
+
+    assert env.body == %{address: "3P37MjBgXvzGhuLWy7h2UqudtaeXDGwvssD"}
   end
 end
