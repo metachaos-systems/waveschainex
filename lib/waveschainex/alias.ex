a6f54fa1e0a45148cfcbe49e60901a007194529d
ontofractal <ontofractal@protonmail.com>
2018-08-10 11:42:46 +0100

    add Alias.alias_of_address fn and test

--- lib/waveschainex/alias.ex
+++ lib/waveschainex/alias.ex
@@ -1,5 +1,6 @@
 defmodule Waveschainex.Alias do
   use Tesla
+
   @doc """
   Returns an address associated with an Alias. Alias should be plain text without an alias prefix and network code.
 
@@ -14,5 +15,21 @@ defmodule Waveschainex.Alias do
           {:ok, Tesla.Env.t()} | {:error, Tesla.Env.t()}
   def address_of_alias(client, alias, _opts \\ []) do
     get(client, "/alias/by-alias/#{alias}")
+  end
+
+  @doc """
+  Returns a collection of aliases associated with an Address
+
+  ## Parameters
+
+  - client : Tesla client
+  - address (String.t): 3Mx2afTZ2KbRrLNbytyzTtXukZvqEB8SkW7
+  - opts: Optional parameters
+
+  """
+  @spec alias_of_address(Tesla.Env.client(), String.t(), keyword()) ::
+          {:ok, Tesla.Env.t()} | {:error, Tesla.Env.t()}
+  def alias_of_address(client, address, _opts \\ []) do
+    get(client, "/alias/by-address/#{address}")
   end
 end
--- test/alias_test.exs
+++ test/alias_test.exs
@@ -15,4 +15,12 @@ defmodule Waveschainex.AliasTest do
 
     assert env.body == %{address: "3PDKVE3GS9mg3a29fdeLUsLHkXvUfQjSZuC"}
   end
+
+  test "get aliases of address", %{client: client} do
+    address = "3PDKVE3GS9mg3a29fdeLUsLHkXvUfQjSZuC"
+
+    {:ok, env} = alias_of_address(client, address)
+
+    assert_value env.body == []
+  end
 end
